d7abab820c4cc88e67ca56a1422184e02465ea86
cjones1047 <cjmj1047@gmail.com>
2022-08-11 16:02:06 -0500

    Lab completed

--- bank.py
+++ bank.py
@@ -1,11 +1,54 @@
-class BankAccount:
-  pass
+from ast import BitAnd
 
-class ChildrensAccount:
-  pass
 
-class OverdraftAccount:
-  pass
+class BankAccount():
+  def __init__(self, balance=0, interest_rate=0.02):
+    self.balance = balance
+    self.interest_rate = interest_rate
+    print("Thanks for creating an account!")
+  def deposit(self, amount):
+    if(amount < 0):
+      return False
+    self.balance += amount
+    print(f"Recent deposit: ${amount}\nNew balance: ${self.balance}")
+    return self.balance
+  def withdraw(self, amount, overdraft_penalty=0):
+    if(amount > self.balance and overdraft_penalty == 0):
+      print(f"Sorry, you can't withdraw ${amount} if your account only has ${self.balance} in it...")
+      return False
+    if(amount > self.balance and overdraft_penalty > 0):
+      print(f"You only have ${self.balance} in your account. Your request to withdraw ${amount} is ignored, and we have charged you an Overdraft Fee of ${overdraft_penalty}")
+      self.balance -= overdraft_penalty
+      return False
+    self.balance -= amount
+    print(f"Recent withdrawal: ${amount}\nNew balance: ${self.balance}")
+    return self.balance
+  def accumulate_interest(self, child=False, overdraft_penalty=0):
+    if(overdraft_penalty > 0 and self.balance < 0):
+      print(f"Your Overdraft Account doesn't accumulate interest since your balance is negative (Account Balance: {self.balance})")
+      return False
+    if(child == True):
+      print(f"Your children's account just tacked on $10 in interest!")
+      self.balance = self.balance + 10
+      return self.balance
+    else:
+      print(f"Your account just accumulated ${self.balance * self.interest_rate} in interest!")
+      self.balance = self.balance + (self.balance * self.interest_rate)
+      return self.balance
+
+class ChildrensAccount(BankAccount):
+  def __init__(self):
+    super().__init__()
+  def accumulate_interest(self, child=True):
+    return super().accumulate_interest(child)
+  
+class OverdraftAccount(BankAccount):
+  def __init__(self):
+    super().__init__()
+  def withdraw(self, amount, overdraft_penalty=40):
+    return super().withdraw(amount, overdraft_penalty)
+  def accumulate_interest(self, child=False, overdraft_penalty=40):
+    return super().accumulate_interest(child, overdraft_penalty)
 
 basic_account = BankAccount()
 basic_account.deposit(600)
